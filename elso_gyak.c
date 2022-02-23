682e9a1ee7436789c49dd62ee76d16b40012a00e
HAkosBalint <zoldfulu1234@gmail.com>
2022-02-23 12:17:08 +0100

    ss

--- elso_gyak.c
+++ elso_gyak.c
@@ -2,9 +2,21 @@
 
 int main(void){
 
-    printf("Hello");
+    printf("Kerlek adj me ey maximum 8 szamjeyu szamot:\n");
 
-    
+    int szam;
+
+    scanf("%d", &szam);
+
+   printf("0-akkal: %.8d\n",szam);
+
+   printf("Szokozokkel:");
+
+   for(int i = 0; i < 8; i++)
+   {
+      printf(" ");
+   }
+   printf("%d",szam);
 
 
 }
